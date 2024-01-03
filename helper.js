4cdf963c4be4c768d23d18f6d09b98fd1584ef8b
noru <nooooru@gmail.com>
2024-01-03 11:19:48 +0800

    fuck

--- helper.js
+++ helper.js
@@ -3,15 +3,12 @@ function getDateString(date = new Date()) {
   let month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date)
   let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
   let hour = date.getHours()
-  let range = '10:00-12:00'
+  let range = '10:00-11:00'
+  if (hour >= 11) {
+    range = '11:00-12:00'
+  }
   if (hour >= 12) {
-    range = '12:00-14:00'
-  }
-  if (hour >= 14) {
-    range = '14:00-16:00'
-  }
-  if (hour >= 16) {
-    range = '16:00-18:00'
+    range = '12:00-13:00'
   }
   return `${year}-${month}-${day} ${range}`
 }
